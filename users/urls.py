ae95486704cc8c53c00a5a3fb0c25c9b8adc1a53
Jose Torres <jose.a.torres513@gmail.com>
2025-05-03 16:39:11 -0400

    Add user signup functionality with form handling and template

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("login/", views.UserLogin.as_view(), name="login"),
+    path("signup/", views.UserSignup.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
+from .forms import SignupForm
+from .models import Profile
+
+""
 
 # Create your views here.
 class UserLogin(LoginView):
@@ -9,3 +13,19 @@ class UserLogin(LoginView):
 
     def get_success_url(self):
         return reverse('home')
+    
+class UserSignup(CreateView):
+    template_name = "users/signup.html"
+    success_url = reverse_lazy('login')
+    form_class = SignupForm
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        passw = form.cleaned_data['password'] # password plain text
+        user.set_password(passw) # hash/encrypt password
+        user.save()
+
+        # Create a profile for the user
+        Profile.objects.create(user=user)
+
+        return super().form_valid(form)
