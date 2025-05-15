0ca556adc8367c0d9d5c464eb956a62818852429
Jose Torres <jose.a.torres513@gmail.com>
2025-05-14 21:56:09 -0400

    Refactor: update blog views and templates for consistency; fix navbar links

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog_home'),
+    path('posts/', views.blog_posts, name='blog_posts'),
+    path('contact/', views.blog_contact, name='blog_contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def blog_home(request):
-    return render(request, 'blog/blog_home.html')
+    return render(request, 'blog/home.html')
+
+def blog_posts(request):
+    return render(request, 'blog/posts.html')
+
+def blog_contact(request):
+    return render(request, 'blog/contact.html')
