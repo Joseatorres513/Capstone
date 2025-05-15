5976723880b702ae5a6572b5154a85114a1f8174
Jose Torres <jose.a.torres513@gmail.com>
2025-05-14 22:30:38 -0400

    Fix: update blog_posts view to render correct template path

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ def blog_home(request):
     return render(request, 'blog/blog_home.html')
 
 def blog_posts(request):
-    return render(request, 'blog/posts.html')
+    return render(request, 'blog/blog_posts.html')
 
 def blog_contact(request):
     return render(request, 'blog/blog_about.html')
